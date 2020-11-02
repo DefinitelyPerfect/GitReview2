df71c1cf7a75a77ced3f6bbdd7acfed60c9d1edf
Osman Onder <honestseller9@gmail.com>
2020-11-02 08:51:20 -0600

    master branchte commit

---
+++ src/Ders1.java
@@ -0,0 +1,12 @@
+
+public class Ders1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Saat 17:52");
+		System.out.println("Saat 18:46");
+		System.out.println("Saat 19:17 - iskender");
+		System.out.println("Saat 19:28 - hamza conflict");
+	}
+
+}
